04950caf695ddef0a9c1b95743e48faf77b70f0f
mrudula-e <62666795+mrudula-e@users.noreply.github.com>
2022-09-13 10:55:21 -0400

    Update FVT.js

--- FVT.js
+++ FVT.js
@@ -1,37 +1,42 @@
 'use strict';
-
-     
-    
-      
+              
     var futureValue;
+    var entryCount = 0;
     
+    
+        
     // get user entries
     //var investment = prompt("Enter investment amount as xxxxx.xx", 10000);
-    var investment = prompt("Enter investment amount as xxxxx.xx")
+    var investment = prompt("Enter investment amount as xxxxx.xx"+
+    "Or enter 0 to end entries", 0);
     investment = parseFloat(investment);
     //document.write("Investment amount = " + investment);
-    document.write('<h2> <i><center>Investment amount<i> <span id = "result"></span>'+investment+' </center></h2>');
-
+    document.write('<h2> <i><center>Investment amount : $ <i> <span id = "result"></span>'+investment+' </center></h2>');
+     
     //var rate = prompt("Enter interest rate as xx.x", 7.5);
-    var rate = prompt("Enter interest rate as xx.x")
+    var rate = prompt("Enter interest rate as xx.x"+
+    "Or enter 0 to end entries", 0);
     rate = parseFloat(rate);
    // document.write(" Interest rate = " + rate);
    document.write('<h2> <i><center>Interest rate<i> <span id = "result"></span>'+rate+' </center></h2>');
 
     //var years = prompt("Enter number of years", 10);
-    var years = prompt("Enter number of years")
+    var years = prompt("Enter number of years"+
+    "Or enter 0 to end entries", 0);
     years = parseInt(years);
     //document.write(" Years = " + years);
     document.write('<h2> <i><center>Years<i> <span id = "result"></span>'+years+' </center></h2>');
-    
+   
     // calulate future value
     futureValue = investment;
     for (var i = 1; i <= years; i++ ) {
-      futureValue = futureValue + (futureValue * rate / 100);
       
-    }
+      futureValue = futureValue + (futureValue * rate / 100);
+     }
+       
     futureValue = parseInt(futureValue);
     //document.write(" Future Value is " + futureValue + "<br><br>");
-    document.write('<h2> <i><center> Future Value is<i> <span id = "result"></span>'+futureValue+' </center></h2>');
+    document.write('<h2> <i><center> Future Value is : $ <i> <span id = "result"></span>'+futureValue+' </center></h2>');
     
-        
+    
+    
